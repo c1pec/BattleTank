c510633be4d335a6bdc0fa2d009fe445b5baf0ae
DESKTOP-6BGS5RJ\c1pec <dev.c1pec@outlook.com>
2018-07-10 14:41:09 +0200

    Add Tick() to TankPlayerController

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -20,8 +20,23 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimTowardsCrosshair();
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location of line-trace through crosshair
+	// If it hits the landscape
+		//Tell controlled tank to aim at this point
 }
